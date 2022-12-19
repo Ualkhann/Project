1a3be55e002ecc9355c704881388ecd271e32b57
Ualkhann <120643949+Ualkhann@users.noreply.github.com>
2022-12-20 03:13:40 +0600

    Add files via upload

--- script0.js
+++ script0.js
@@ -12,3 +12,19 @@ if(close){
        nav.classList.remove('active');
     })
 }
+
+var MainImage=document.getElementById("MainImage");
+var smallImage=document.getElementsByClassName("small-image");
+
+smallImage[0].onclick=function(){
+    MainImage.src=smallImage[0].src;
+}
+smallImage[1].onclick=function(){
+    MainImage.src=smallImage[1].src;
+}
+smallImage[2].onclick=function(){
+    MainImage.src=smallImage[2].src;
+}
+smallImage[3].onclick=function(){
+    MainImage.src=smallImage[3].src;
+}
