489bb15773ae6f58e02ba7a4c11f9ecea8995fd9
Dauylkyzy <121027946+Dauylkyzy@users.noreply.github.com>
2022-12-20 12:43:35 +0600

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,3 @@
+document.getElementById("images").src = "./images/WhatsApp Image 2022-12-19 at 16.01.19.jpeg";
+document.getElementById("images").src = "./images/WhatsApp Image 2022-12-19 at 16.04.59.jpeg";
+document.getElementById("images").src = "./images/WhatsApp Image 2022-12-19 at 16.01.01.jpeg";
